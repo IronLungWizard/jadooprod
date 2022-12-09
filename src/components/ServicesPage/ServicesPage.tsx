dcae27f70f6febaf6ed720ecacc1d24c0da23517
IronLungWizard <3967070@mail.ru>
2022-12-09 12:40:33 +0300

    fixed

--- src/components/ServicesPage/ServicesPage.tsx
+++ src/components/ServicesPage/ServicesPage.tsx
@@ -10,40 +10,40 @@ import CustomizationServiceVector from '../../../src/images/CustomizationService
 const ServicesPage = () => {
   return (
     <div className="servicesPage">
-      <p className="spanCategory">CATEGORY</p>
-      <h2 className="spanWeOfferBestServices">We Offer Best Services</h2>
-      <article className="services">
-        <div className="services__calculatedWeather">
-          <img className="weatherServiceVector" src={WeatherServiceVector} alt=""/>
-          <div className="serviceInfo">
-            <h3 className="spanServiceName">Calculated Weather</h3>
-            <p className="spanServiceDescription">Built Wicket longer admire do barton vanity itself do in it.</p>
-          </div>
-        </div>
-        <div className="services__bestFlights">
-          <img className="flightServiceVector" src={FlightServiceVector} alt=""/>
-          <div className="serviceInfo">
-            <h3 className="spanServiceName">Best Flights</h3>
-            <p className="spanServiceDescription">Engrossed listening. Park gate sell they west hard for the.</p>
-          </div>
-          <img className="bestFlightRectangle" src={BestFlightRectangle} alt=""/>
-        </div>
-        <div className="services__localEvents">
-          <img className="eventsServiceVector" src={EventsServiceVector} alt=""/>
-          <div className="serviceInfo">
-            <h3 className="spanServiceName">Local Events</h3>
-            <p className="spanServiceDescription">Barton vanity itself do in it. Preferd to men it engrossed listening. </p>
-          </div>
-        </div>
-        <div className="services__customization">
-          <img className="customizationServiceVector" src={CustomizationServiceVector} alt=""/>
-          <div className="serviceInfo">
-            <h3 className="spanServiceName">Customization</h3>
-            <p className="spanServiceDescription">We deliver outsourced aviation services for military customers.</p>
-          </div>
-        </div>
-      </article>
-  </div>
+    <p className="spanCategory">CATEGORY</p>
+    <h2 className="spanWeOfferBestServices">We Offer Best Services</h2>
+    <article className="services">
+      <div className="services__calculatedWeather">
+        <img className="weatherServiceVector" src={WeatherServiceVector} alt=""/>
+        <section className="serviceInfo">
+          <h3 className="spanServiceName">Calculated Weather</h3>
+          <p className="spanServiceDescription">Built Wicket longer admire do barton vanity itself do in it.</p>
+        </section>
+      </div>
+      <div className="services__bestFlights">
+        <img className="flightServiceVector" src={FlightServiceVector} alt=""/>
+        <section className="serviceInfo">
+          <h3 className="spanServiceName">Best Flights</h3>
+          <p className="spanServiceDescription">Engrossed listening. Park gate sell they west hard for the.</p>
+        </section>
+        <img className="bestFlightRectangle" src={BestFlightRectangle} alt=""/>
+      </div>
+      <div className="services__localEvents">
+        <img className="eventsServiceVector" src={EventsServiceVector} alt=""/>
+        <section className="serviceInfo">
+          <h3 className="spanServiceName">Local Events</h3>
+          <p className="spanServiceDescription">Barton vanity itself do in it. Preferd to men it engrossed listening. </p>
+        </section>
+      </div>
+      <div className="services__customization">
+        <img className="customizationServiceVector" src={CustomizationServiceVector} alt=""/>
+        <section className="serviceInfo">
+          <h3 className="spanServiceName">Customization</h3>
+          <p className="spanServiceDescription">We deliver outsourced aviation services for military customers.</p>
+        </section>
+      </div>
+    </article>
+</div>
   )
 }
 
